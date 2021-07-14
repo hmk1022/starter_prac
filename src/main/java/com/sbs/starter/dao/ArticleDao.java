83674f9baed7fbdd6cde8f8b5a7e1e1b6106ccbc
hmk1022 <hm.kim@iworkerman>
2021-07-14 18:16:02 +0900

    dao 에러 해결

--- src/main/java/com/sbs/starter/dao/ArticleDao.java
+++ src/main/java/com/sbs/starter/dao/ArticleDao.java
@@ -6,7 +6,8 @@ import org.apache.ibatis.annotations.Mapper;
 
 import com.sbs.starter.dto.Article;
 
-@Mapper // 이렇게 해주면 ArticleDao의 구현체를 마이바티스가 대신 구현해준다
+
+@Mapper
 public interface ArticleDao {
 	
 	public List<Article> getList();
